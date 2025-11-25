657b735ff94b95632c0ac798727c2ef25ad23834
Mohamed Hassan <mohamedwahieb57@gmail.com>
2025-11-25 20:25:19 +0200

    commit by abohassan

---
+++ Korik.Infrastructure/Migrations/20251125162610_Adapting the Database to the Add Service Component.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Korik.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdaptingtheDatabasetotheAddServiceComponent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Subcategories");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Services");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Subcategories",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Services",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
