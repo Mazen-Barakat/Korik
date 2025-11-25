df2850d30ccc49cd8d5adab10e3f6ee26f795683
ahmed-aliii <ahmedmah1284@gmail.com>
2025-11-25 17:24:19 +0200

    Understood! Please provide the list of code changes, and I'll craft a concise and comprehensive commit message for you.

--- Korik.Application/DTOs/WorkShopWorkingHours/Request_DTOs/CreateWorkShopWorkingHoursDTO/CreateWorkShopWorkingHoursDTOValidator.cs
+++ Korik.Application/DTOs/WorkShopWorkingHours/Request_DTOs/CreateWorkShopWorkingHoursDTO/CreateWorkShopWorkingHoursDTOValidator.cs
@@ -10,28 +10,37 @@ namespace Korik.Application
     public class CreateWorkShopWorkingHoursDTOValidator : AbstractValidator<CreateWorkShopWorkingHoursDTO>
     {
         private readonly IWorkShopWorkingHoursService _workingHoursService;
+        private readonly IWorkShopProfileService _workShopProfileService;
 
-        public CreateWorkShopWorkingHoursDTOValidator(IWorkShopWorkingHoursService workingHoursService)
+        public CreateWorkShopWorkingHoursDTOValidator
+            (
+            IWorkShopWorkingHoursService workingHoursService, 
+            IWorkShopProfileService workShopProfileService
+            )
         {
             _workingHoursService = workingHoursService;
+            _workShopProfileService = workShopProfileService;
+
 
             RuleFor(x => x.Day)
                 .IsInEnum().WithMessage("Day must be a valid day of the week.");
 
             RuleFor(x => x.From)
-                .NotEmpty().WithMessage("From time is required.")
-                .Must((model, from) => !model.IsClosed || from == TimeOnly.MinValue)
-                .WithMessage("From time should be empty when workshop is closed.");
+                .NotEmpty().WithMessage("From time is required.");
 
             RuleFor(x => x.To)
                 .NotEmpty().WithMessage("To time is required.")
-                .Must((model, to) => !model.IsClosed || to == TimeOnly.MinValue)
-                .WithMessage("To time should be empty when workshop is closed.")
                 .Must((model, to) => model.IsClosed || to > model.From)
                 .WithMessage("To time must be after From time.");
 
             RuleFor(x => x.WorkShopProfileId)
-                .GreaterThan(0).WithMessage("Workshop profile ID is required.");
+                .GreaterThan(0).WithMessage("Workshop profile ID is required.")
+                .MustAsync(async (id, cancellationToken) =>
+                {
+                    var result = await _workShopProfileService.IsExistAsync(id);
+                    return result.Data;   // must be bool
+                })
+                .WithMessage("Workshop profile does not exist.");
 
             RuleFor(x => x)
                 .MustAsync(async (model, cancellationToken) =>
