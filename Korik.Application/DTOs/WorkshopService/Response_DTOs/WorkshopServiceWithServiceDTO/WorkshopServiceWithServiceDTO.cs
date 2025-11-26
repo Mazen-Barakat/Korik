8a48e2af2b0101e151c38b49a34e447cbf79a294
Mazen Barakat <mazen1072000@gmail.com>
2025-11-26 16:10:41 +0200

    Add API to fetch workshop services by profile ID
    Introduced a new API endpoint `Get-Workshop-Services-By-Profile-ID` to fetch workshop services by workshop profile ID. Added supporting DTOs, validation, and MediatR request/handler for processing. Enhanced AutoMapper profiles for new mappings.
    
    Updated `Korik.API.csproj` to remove unused `Controllers\Database\` files and updated the target framework to `net9.0`. Modified `appsettings.json` to update the database connection string.
    
    Deleted unused binary image files to streamline the repository. Cleaned up unused code in `SearchWorkshopsByServiceAndOriginDTOValidator.cs`.

---
+++ Korik.Application/DTOs/WorkshopService/Response_DTOs/WorkshopServiceWithServiceDTO/WorkshopServiceWithServiceDTO.cs
@@ -0,0 +1,25 @@
+﻿using Korik.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Korik.Application
+{
+    public class WorkshopServiceWithServiceDTO
+    {
+        public int Id { get; set; }
+        public int ServiceId { get; set; }
+        public int WorkShopProfileId { get; set; }
+        public int Duration { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public CarOrigin Origin { get; set; }
+
+        // Service Details (from Service table)
+        public string ServiceName { get; set; }
+
+        public string ServiceDescription { get; set; }
+    }
+}
